5ab9eda716b89fddcd8b58567c1c9ba43a1f0b9e
John <jddhalogamer@gmail.com>
2020-12-21 18:57:13 -0600

    clock build

---
+++ clock.js
@@ -0,0 +1,25 @@
+// Functions
+function currentTime() {
+    let date = new Date();
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    hour = updateTime(hour);
+    min = updateTime(min);
+    sec = updateTime(sec);
+    let midday = "AM";
+    midday = (hour >= 12) ? "PM" : "AM";
+    hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12): hour);
+    document.getElementById("clock").innerHTML = hour + " : " + min + " : " + sec;
+    let t = setTimeout(function() { currentTime() }, 1000);
+}
+
+function updateTime(k) {
+    if (k < 10) {
+        return "0" + k;
+    } else {
+        return k;
+    }
+}
+
+currentTime();
